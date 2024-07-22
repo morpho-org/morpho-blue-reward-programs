2167ded973d2feba8b036b458130d85b6e006cda
Ivan Camps <ivanca@sas.upenn.edu>
2024-07-22 10:34:37 -0700

    Reserve borrow incentives bsdETH/ETH market Base

--- src/market-programs.ts
+++ src/market-programs.ts
@@ -657,4 +657,19 @@ export const marketPrograms: MarketRewardProgramArgs[] = [
     },
     chainId: ChainId.MAINNET,
   },
+  // bsdETH/ETH Base 600,000 RSR 07/25/2024 08/25/2024 1pm GMT
+  {
+    start: 1721912410n, 
+    end: 1724590810n, 
+    fundsSender: "0x03d03A026E71979BE3b08D44B01eAe4C5FF9da99",
+    urdAddress: "0x5400dBb270c956E8985184335A1C62AcA6Ce1333",
+    tokenAddress: "0xaB36452DbAC151bE02b16Ca17d8919826072f64a",
+    marketId: "0xdf6aa0df4eb647966018f324db97aea09d2a7dde0d3c0a72115e8b20d58ea81f",
+    rewardAmount: {
+      supply: 0n,
+      borrow: parseUnits("600000", 18),
+      collateral: 0n,
+    },
+    chainId: ChainId.BASE,
+  },
 ];
