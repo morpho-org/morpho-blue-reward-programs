d7415944bd78a84497ff46dc912fbba4407605ad
Ivan Camps <ivanca@sas.upenn.edu>
2024-07-22 16:35:30 -0700

    Reserve borrow incentives bsdETH/ETH market Base

--- src/vault-programs.ts
+++ src/vault-programs.ts
@@ -110,13 +110,8 @@ export const vaultPrograms: VaultRewardProgramArgs[] = [
   },
   // Re7 eUSD Vault - 8,000,000 RSR 07/22-08/22 4pm GMT
   {
-<<<<<<< HEAD
     start: 1721664000n, 
     end: 1724342400n, 
-=======
-    start: 1721664000n,
-    end: 1724342400n,
->>>>>>> upstream/main
     fundsSender: "0x03d03A026E71979BE3b08D44B01eAe4C5FF9da99",
     urdAddress: "0x5400dbb270c956e8985184335a1c62aca6ce1333",
     tokenAddress: "0xaB36452DbAC151bE02b16Ca17d8919826072f64a",
@@ -126,13 +121,8 @@ export const vaultPrograms: VaultRewardProgramArgs[] = [
   },
   // Guntlet eUSD Core Vault - 11,000,000 RSR 07/22-08/22 4pm GMT
   {
-<<<<<<< HEAD
     start: 1721664000n, 
     end: 1724342400n, 
-=======
-    start: 1721664000n,
-    end: 1724342400n,
->>>>>>> upstream/main
     fundsSender: "0x03d03A026E71979BE3b08D44B01eAe4C5FF9da99",
     urdAddress: "0x330eefa8a787552DC5cAd3C3cA644844B1E61Ddb",
     tokenAddress: "0x320623b8E4fF03373931769A31Fc52A4E78B5d70",
