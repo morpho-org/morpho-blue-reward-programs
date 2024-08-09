4032579ffd483bd478af4ecfbb67050dd6f08232
Dev <gabinhry@gmail.com>
2024-08-09 14:26:33 +0200

    fix: remove airdrop dev

--- src/airdrop-programs.ts
+++ src/airdrop-programs.ts
@@ -23,15 +23,4 @@ export type AirdropRewardProgramArgs = {
  *   }
  * ]
  */
-export const airdropPrograms: AirdropRewardProgramArgs[] = [
-  // This program is for Development purposes only
-  // DO NOT PUSH AIRDROP PROGRAMS TO PRODUCTION WITHOUT REMOVING IT
-  {
-    claimDate: 1717149233n,
-    fundsSender: "0xF057afeEc22E220f47AD4220871364e9E828b2e9",
-    urdAddress: "0x330eefa8a787552dc5cad3c3ca644844b1e61ddb",
-    tokenAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-    ipfsHash: "QmUSNh34dpKFbX7YMU4fd5JbfkVYzb4e45o3pSqqXvzkKw",
-    chainId: ChainId.MAINNET,
-  },
-];
+export const airdropPrograms: AirdropRewardProgramArgs[] = [];
