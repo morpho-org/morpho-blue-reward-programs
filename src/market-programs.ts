0582ea77c3d31a71601c1c27860d2ffce97a86c1
Reppelin Tom <tomireppelin@gmail.com>
2024-10-04 11:00:00 +0200

    feat(PAXG): lint

--- src/market-programs.ts
+++ src/market-programs.ts
@@ -1160,7 +1160,7 @@ export const marketPrograms: MarketRewardProgramArgs[] = [
     tokenAddress: "0x45804880De22913dAFE09f4980848ECE6EcbAf78",
     marketId: "0x8eaf7b29f02ba8d8c1d7aeb587403dcb16e2e943e4e2f5f94b0963c2386406c9",
     rewardAmount: {
-      supply:  3150000000000000000n,
+      supply: 3150000000000000000n,
       borrow: 0n,
       collateral: 0n,
     },
