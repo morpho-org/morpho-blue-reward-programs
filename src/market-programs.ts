de0537b6ae288971ef993a525769568aea600cdd
hassan <hassanashraf8888@gmail.com>
2024-10-09 14:06:20 +0530

    add title and urdAddress

--- src/market-programs.ts
+++ src/market-programs.ts
@@ -1181,11 +1181,13 @@ export const marketPrograms: MarketRewardProgramArgs[] = [
     },
     chainId: ChainId.MAINNET,
   },
+  // Add rsETH/WETH incentives 15/10/2024 - 28/10/2024
   {
     start: 1728930600n,
     end: 1730053800n,
     fundersSender: "0x9cCC7Df2da448BBeA48347822E2A920053B7E67B"
     tokenAddress: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    urdAddress: "0x9cCC7Df2da448BBeA48347822E2A920053B7E67B",
     marketId: "0xba761af4134efb0855adfba638945f454f0a704af11fc93439e20c7c5ebab942",
     rewardAmount: {
       supply: parseUnits("2500", 6),
