e9c3ca36fd2c66dcfd4f9e653a427211e2d28588
Val <98047648+0xValJohn@users.noreply.github.com>
2024-10-24 08:55:51 -0300

    feat: crv incentives for crvUSD vault epoch 7
    Signed-off-by: Val <98047648+0xValJohn@users.noreply.github.com>

--- src/vault-programs.ts
+++ src/vault-programs.ts
@@ -350,4 +350,15 @@ export const vaultPrograms: VaultRewardProgramArgs[] = [
     amount: parseUnits("20000", 18),
     chainId: ChainId.BASE,
   },
+  // LlamaRisk crvUSD Vault (Epoch 7) - 15_000 CRV over 4 weeks
+  {
+    start: 1730480708n, // Fri Nov 01 2024 17:05:08 GMT+0000
+    end: 1732899908n, // Fri Nov 29 2024 17:05:08 GMT+0000
+    fundsSender: "0x7e246fAce452AC43F5dC60c41EE14C88c37951c6", // LlamaRisk aggregator multisig
+    urdAddress: "0x330eefa8a787552DC5cAd3C3cA644844B1E61Ddb", // mainnet urd
+    tokenAddress: "0xD533a949740bb3306d119CC777fa900bA034cd52", // crv
+    vault: "0x67315dd969B8Cd3a3520C245837Bf71f54579C75", // LlamaRisk crvUSD Vault
+    amount: parseUnits("15_000", 18),
+    chainId: ChainId.MAINNET,
+  },
 ];
