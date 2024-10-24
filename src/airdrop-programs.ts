6e1d78af6145be537f172ce666573948d4fc7e59
Dev <gabinhry@gmail.com>
2024-10-24 11:19:00 +0200

    feat: re-funds pyth

--- src/airdrop-programs.ts
+++ src/airdrop-programs.ts
@@ -1,3 +1,5 @@
+import { parseUnits } from "viem";
+
 import { Address, ChainId } from "@morpho-org/blue-rewards-core-sdk";
 
 export type AirdropRewardProgramArgs = {
@@ -7,6 +9,7 @@ export type AirdropRewardProgramArgs = {
   tokenAddress: Address;
   chainId: ChainId;
   cidV0: string;
+  totalRewards: bigint;
 };
 
 /**
@@ -19,7 +22,8 @@ export type AirdropRewardProgramArgs = {
  *    urdAddress: "0x330eefa8a787552dc5cad3c3ca644844b1e61ddb",
  *    tokenAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
  *    cidV0: "QmUSNh34dpKFbX7YMU4fd5JbfkVYzb4e45o3pSqqXvzkKw",
- *    chainId: ChainId.MAINNET
+ *    chainId: ChainId.MAINNET,
+ *    totalRewards: 50000n
  *   }
  * ]
  */
@@ -31,5 +35,15 @@ export const airdropPrograms: AirdropRewardProgramArgs[] = [
     tokenAddress: "0x9994E35Db50125E0DF82e4c2dde62496CE330999",
     cidV0: "QmbfETB4KZG96tjfPZLJHb8Xn1bjNbfK9vyUcdgfsm33WG",
     chainId: ChainId.MAINNET,
+    totalRewards: parseUnits("600000", 18),
+  },
+  {
+    claimDate: 1729771200n,
+    fundsSender: "0xF057afeEc22E220f47AD4220871364e9E828b2e9",
+    urdAddress: "0x330eefa8a787552dc5cad3c3ca644844b1e61ddb",
+    tokenAddress: "0xeFc0CED4B3D536103e76a1c4c74F0385C8F4Bdd3",
+    cidV0: "Qmbb3iTjUa28n7699HFGo8pHvSqCx1sqN1L7aDBAp7685n",
+    chainId: ChainId.MAINNET,
+    totalRewards: parseUnits("50000", 6),
   },
 ];
