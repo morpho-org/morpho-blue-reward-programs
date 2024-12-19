26549072f66a2bb3bfe056a363f7a043782e3401
arcc0s <97156729+arcc0s@users.noreply.github.com>
2024-12-19 14:02:24 +0100

    new pendle wbtc reward program (#94)

--- src/vault-programs.ts
+++ src/vault-programs.ts
@@ -668,4 +668,16 @@ export const vaultPrograms: VaultRewardProgramArgs[] = [
     amount: parseUnits("1350000", 18),
     chainId: ChainId.BASE,
   },
+
+  // Pendle WBTC Vault - 2000 Pendle 12/23-01/18 6pm GMT
+  {
+    start: 1734976800n,
+    end: 1737223200n,
+    fundsSender: "0x650741eB4f6AB0776B9bF98A3280E3Cd6A2F1BF1", // mev capital ms 2
+    urdAddress: "0x330eefa8a787552DC5cAd3C3cA644844B1E61Ddb", // mainnet urd
+    tokenAddress: "0x808507121B80c02388fAd14726482e061B8da827", // Pendle token
+    vault: "0x2f1aBb81ed86Be95bcf8178bA62C8e72D6834775", // Pendle WBTC vault
+    amount: parseUnits("2000", 18),
+    chainId: ChainId.MAINNET,
+  },
 ];
