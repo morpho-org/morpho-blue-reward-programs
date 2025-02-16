7a02856f7b0a5836ee7752c3afc585b2888ef5f4
Val <98047648+0xValJohn@users.noreply.github.com>
2025-02-16 08:10:48 -0500

    fix: start date
    Signed-off-by: Val <98047648+0xValJohn@users.noreply.github.com>

--- src/vault-programs.ts
+++ src/vault-programs.ts
@@ -903,23 +903,13 @@ export const vaultPrograms: VaultRewardProgramArgs[] = [
   },
   // LlamaRisk crvUSD Vault (Epoch 9) - 15_000 CRV over 4 weeks
   {
-    start: 1739768400n, // Mon Feb 17 2025 05:00:00 GMT+0000
-    end: 1742187600n, // 	Mon Mar 17 2025 05:00:00 GMT+0000
+    start: 1740373200n, // Mon Feb 24 2025 05:00:00 GMT+0000
+    end: 1742788800n, // 	Mon Mar 24 2025 04:00:00 GMT+0000
     fundsSender: "0x7e246fAce452AC43F5dC60c41EE14C88c37951c6", // LlamaRisk aggregator multisig
     urdAddress: "0x330eefa8a787552DC5cAd3C3cA644844B1E61Ddb", // mainnet urd
     tokenAddress: "0xD533a949740bb3306d119CC777fa900bA034cd52", // crv
     vault: "0x67315dd969B8Cd3a3520C245837Bf71f54579C75", // LlamaRisk crvUSD Vault
     amount: parseUnits("15000", 18),
     chainId: ChainId.MAINNET,
-  },
-
-
-
-
-
-
-
-
-
-  
+  }
 ];
