08e71c52f6e147cd0be4ba1d7ddd0e1a6c812f07
Yassin El Idrissi <yassinelidrissi1995@gmail.com>
2025-03-24 18:12:40 +0300

    sUSDS on base week 4 program

--- src/market-programs.ts
+++ src/market-programs.ts
@@ -1826,6 +1826,21 @@ export const marketPrograms: MarketRewardProgramArgs[] = [
     },
     chainId: ChainId.BASE,
   },
+  // wstETH/sUSDS 12,500 USDS on Base 3/31/2025 04/07/2025 0am UTC
+  {
+    start: 1743379200n,
+    end: 1743984000n,
+    fundsSender: "0x1Fe9F102593861118D8664E84201ec9754198F30",
+    urdAddress: "0x5400dbb270c956e8985184335a1c62aca6ce1333",
+    tokenAddress: "0x820C137fa70C8691f0e44Dc420a5e53c168921Dc",
+    marketId: "0xb003f1eddcaece702f8b34d684840ef913e00d6c9b0e209f67f8524bfc095733",
+    rewardAmount: {
+      supply: parseUnits("12500", 18),
+      borrow: 0n,
+      collateral: 0n,
+    },
+    chainId: ChainId.BASE,
+  },
   // cbBTC/sUSDS 12,500 USDS on Base 3/31/2025 04/07/2025 0am UTC
   {
     start: 1743379200n,
